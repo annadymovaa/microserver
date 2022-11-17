e14981c58025d7dd9e9a0660d3f712347002553f
annadymovaa <aanadumova@hse.edu.ru>
2022-11-18 00:20:40 +0500

    final for today?

--- http-rest-api/go.mod
+++ http-rest-api/go.mod
@@ -11,9 +11,17 @@ require (
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+)
+
+require (
 	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-migrate/migrate v3.5.4+incompatible
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/sessions v1.2.1
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- http-rest-api/inetrnal/app/store/repository.go
+++ http-rest-api/inetrnal/app/store/repository.go
@@ -15,7 +15,7 @@ func (r *Repository) CreateBalance(a *account.Account) (*account.Account, error)
 	// 	// 	return err
 	// 	// }
 
-	if err := r.store.db.QueryRow("INSERT INTO accounts(amount, type_acc) VALUES ($1, 'balance') RETURNING id_account", a.Amount).Scan(&a.Id_account); err != nil {
+	if err := r.store.db.QueryRow("INSERT INTO accounts(amount, type_acc) VALUES ($1, 'balance') RETURNING id_account;", a.Amount).Scan(&a.Id_account); err != nil {
 		return nil, err
 	}
 
@@ -24,20 +24,35 @@ func (r *Repository) CreateBalance(a *account.Account) (*account.Account, error)
 
 func (r *Repository) FindByUser(id_user int) (*account.Account, error) {
 	a := &account.Account{}
-	if err := r.store.db.QueryRow("select amount from accounts where id_user = $1 and type_acc = 'balance'", id_user).Scan(&a.Amount); err != nil {
+	if err := r.store.db.QueryRow("select amount from accounts where id_user = $1 and type_acc = 'balance';", id_user).Scan(&a.Amount); err != nil {
 		return nil, err
 	}
 	return a, nil
 }
 
-// func (r *Repository) ReserveMoney(id_user int, id_service uint64, id_order uint64, price int) (*model.Account, error) {
-// 	a := &model.Account{}
-// 	ser := &model.Service{}
-// 	o := &model.Order{}
-// 	balance := r.FindByUser(id_user)
+func (r *Repository) ReserveMoney(id_user int, id_service uint64, id_order uint64, price int) (*account.Account, error) {
 
-// 	if err := r.store.db.QueryRow(""); err != nil{
-// 		return nil, err
-// 	}
-// 	return nil, nil
-// }
+	a := &account.Account{}
+	// 	ser := &model.Service{}
+	// 	o := &model.Order{}
+	//balance := r.store.db.QueryRow("select amount from accounts where id_user = $1 and type_acc = 'balance';", id_user).Scan(&a.Amount)
+
+	userid := r.store.db.QueryRow("SELECT amount, id_account, type_acc FROM accounts where id_user=$1;", id_user).Scan()
+
+	if err := userid; err != nil {
+		return nil, err
+	}
+
+	summa := r.store.db.QueryRow("SELECT price FROM services WHERE id_service=$1;", id_service).Scan()
+
+	if err := summa; err != nil {
+		return nil, err
+	}
+
+	//unreserved = balance - summa
+	if err := r.store.db.QueryRow("UPDATE accounts SET amount=amount-price WHERE id_user=$1 and type_acc = 'balance';", id_user).Scan(); err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
